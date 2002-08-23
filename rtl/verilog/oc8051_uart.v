9c6129b64ec2379504c1ae17a22e5cd29a3c477a
simont <simont@a4e7ce1a-ff1c-4833-a393-125ca6a07d6f>
2002-08-23 12:03:43 +0000

    mode 1 and 3 divide clooak with 31 or 16, mode 2 with 64 or 32

--- rtl/verilog/oc8051_uart.v
+++ rtl/verilog/oc8051_uart.v
@@ -16,21 +16,17 @@ output [7:0] data_out;
 reg txd, bit_out;
 reg [7:0] data_out;
 
-reg tr_start, trans, trans_buf, t1_ow_buf, smod_cnt_t, smod_cnt_r, re_start;
+reg tr_start, trans, trans_buf, t1_ow_buf;
+reg [5:0] smod_cnt_r, smod_cnt_t; 
 reg receive, receive_buf, rxd_buf, r_int;
 //
-// mode 2 counter
-reg [2:0] mode2_count;
 reg [7:0] sbuf_rxd, sbuf_txd, scon, pcon;
 reg [10:0] sbuf_rxd_tmp;
 //
 //tr_count	trancive counter
 //re_count	receive counter
-reg [3:0] tr_count, re_count;
+reg [3:0] tr_count, re_count, re_count_buff;
 
-//
-// sam_cnt	sample counter
-reg [2:0] sam_cnt, sample;
 
 assign intr = scon[1] | scon [0];
 
@@ -82,8 +78,7 @@ begin
     txd <= #1 1'b1;
     tr_count <= #1 4'd0;
     trans <= #1 1'b0;
-    smod_cnt_t <= #1 1'b0;
-    mode2_count <= #1 3'b000;
+    smod_cnt_t <= #1 6'h0;
 //
 // start transmiting
 //
@@ -102,10 +97,9 @@ begin
       end
     endcase
     trans <= #1 1'b1;
-    smod_cnt_t <= #1 1'b0;
-    mode2_count <= #1 3'b000;
+    smod_cnt_t <= #1 6'h0;
 //
-// transmiting
+// transmiting/
 //
   end else if (trans)
   begin
@@ -122,47 +116,50 @@ begin
       end
       2'b01: begin // mode 1
         if ((t1_ow) & !(t1_ow_buf))
-	begin
-	  if ((pcon[7]) | (smod_cnt_t))
-	  begin
+       	begin
+	        if (((pcon[7]) &  (smod_cnt_t == 6'd15))| (!(pcon[7]) & (smod_cnt_t==6'd31)))
+	        begin
             case (tr_count)
               4'd8: txd <= #1 1'b1;  // stop bit
-	      4'd9: trans <= #1 1'b0;
-	      4'b1111: txd <= #1 1'b0; //start bit
-	      default: txd <= #1 sbuf_txd[tr_count];
-	    endcase
+	            4'd9: trans <= #1 1'b0;
+	            4'b1111: txd <= #1 1'b0; //start bit
+	            default: txd <= #1 sbuf_txd[tr_count];
+	          endcase
             tr_count <= #1 tr_count + 4'b1;
-	    smod_cnt_t <= #1 1'b0;
-	  end else smod_cnt_t <= #1 1'b1;
-	end
+	          smod_cnt_t <= #1 6'h0;
+	        end else smod_cnt_t <= #1 smod_cnt_t + 6'h01;
+	      end
       end
       2'b10: begin // mode 2
 //
 // if smod (pcon[7]) is 1 count to 4 else count to 6
 //
-        if (((pcon[7]) & (mode2_count==3'b011)) | (!(pcon[7]) & (mode2_count==3'b101))) begin
-	  case (tr_count)
+        if (((pcon[7]) & (smod_cnt_t==6'd31)) | (!(pcon[7]) & (smod_cnt_t==6'd63))) begin
+     	    case (tr_count)
             4'd8: begin
-	      txd <= #1 scon[3];
-	    end
+	            txd <= #1 scon[3];
+	          end
             4'd9: begin
-	      txd <= #1 1'b1; //stop bit
-	      trans <= #1 1'b0;
-	    end
-	    default: begin
-	      txd <= #1 sbuf_txd[tr_count];
-	    end
-	  endcase
+	            txd <= #1 1'b1; //stop bit
+	          end
+            4'd10: begin
+	            trans <= #1 1'b0;
+	          end
+            
+	          default: begin
+	            txd <= #1 sbuf_txd[tr_count];
+	          end
+	        endcase
           tr_count <= #1 tr_count+1'b1;
-	  mode2_count <= #1 3'd0;
-	end else begin
-          mode2_count <= #1 mode2_count + 3'b1;
-	end
+	        smod_cnt_t <= #1 6'h00;
+	      end else begin
+          smod_cnt_t <= #1 smod_cnt_t + 6'h01;
+	      end
       end
       default: begin // mode 3
         if ((t1_ow) & !(t1_ow_buf))
 	begin
-	  if ((pcon[7]) | (smod_cnt_t))
+      if (((pcon[7]) &  (smod_cnt_t == 6'd15))| (!(pcon[7]) & (smod_cnt_t==6'd31)))
 	  begin
 	    case (tr_count)
               4'd8: begin
@@ -180,8 +177,8 @@ begin
 	      end
 	    endcase
             tr_count <= #1 tr_count+1'b1;
-	    smod_cnt_t <= #1 1'b0;
-	  end else smod_cnt_t <= #1 1'b1;
+	    smod_cnt_t <= #1 6'h00;
+	  end else smod_cnt_t <= #1 smod_cnt_t + 6'h01;
 	end
       end
     endcase
@@ -207,15 +204,12 @@ end
 always @(posedge clk or posedge rst)
 begin
   if (rst) begin
-    sample <= #1 3'b000;
-    sam_cnt <= #1 3'b000;
     re_count <= #1 4'd0;
     receive <= #1 1'b0;
     sbuf_rxd <= #1 8'h00;
     sbuf_rxd_tmp <= #1 11'd0;
-    smod_cnt_r <= #1 1'b0;
+    smod_cnt_r <= #1 6'h00;
     r_int <= #1 1'b0;
-    re_start <= #1 1'b0;
   end else if (receive) begin
     case (scon[7:6])
       2'b00: begin // mode 0
@@ -231,119 +225,92 @@ begin
       end
       2'b01: begin // mode 1
         if ((t1_ow) & !(t1_ow_buf))
-	begin
-	  if ((pcon[7]) | (smod_cnt_r))
-	  begin
-            sam_cnt <= #1 3'b000;
+        begin
+          if (((pcon[7]) &  (smod_cnt_r == 6'd15))| (!(pcon[7]) & (smod_cnt_r==6'd31)))
+	        begin
             r_int <= #1 1'b0;
+      	    re_count <= #1 re_count + 4'd1;
+            smod_cnt_r <= #1 6'h00;
+            sbuf_rxd_tmp[re_count_buff] <= #1 rxd;
+            if ((re_count==4'd0) && (rxd)) 
+              receive <= #1 1'b0;
 
-	    re_count <= #1 re_count + 4'd1;
-	    smod_cnt_r <= #1 1'b0;
-	  end else smod_cnt_r <= #1 1'b1;
-	end else begin
-	  if (sam_cnt==3'b011) begin
-	    if ((sample[0] ^ sample[1]) | (sample[0] ^ sample[2]))
-	      sbuf_rxd_tmp[re_count] <= #1 sample[0];
-	    else
-	      sbuf_rxd_tmp[re_count] <= #1 sample[1];
-	    if (re_count == 4'd9)
-	    begin
-	      sbuf_rxd <= #1 sbuf_rxd_tmp[8:1];
-	      receive <= #1 1'b0;
-	      r_int <= #1 1'b1;
-	    end else r_int <= #1 1'b0;
-	  end else begin
-	    sample[sam_cnt[1:0]] <= #1 rxd;
-	    sam_cnt <= #1 sam_cnt +1'b1;
-	    r_int <= #1 1'b0;
-	  end
-	end
+	        end else smod_cnt_r <= #1 smod_cnt_r + 6'h01;
+	      end else begin 
+   	      r_int <= #1 1'b1;
+     	    if (re_count == 4'd10)
+          begin
+     	      sbuf_rxd <= #1 sbuf_rxd_tmp[8:1];
+            receive <= #1 1'b0;
+     	      r_int <= #1 1'b1;
+          end else r_int <= #1 1'b0;
+        end
       end
       2'b10: begin // mode 2
-        if (((pcon[7]) & (sam_cnt==3'b100)) | (!(pcon[7]) & (sam_cnt==3'b110))) begin
-	  if (re_count==4'd11) begin
-	      sbuf_rxd <= #1 sbuf_rxd_tmp[8:1];
-	      r_int <= #1 sbuf_rxd_tmp[0] | !scon[5];
-	      receive <= #1 1'b0;
-	  end else begin
-	    sam_cnt <= #1 3'b001;
-	    sample[0] <= #1 rxd;
-	    r_int <= #1 1'b0;
-	  end
-    re_count <= #1 re_count + 4'd1;
-	end else begin
-	  r_int <= #1 1'b0;
-
-	  if (sam_cnt==3'b011) begin
-	    if ((sample[0] ^ sample[1]) | (sample[0] ^ sample[2]))
-	      sbuf_rxd_tmp[re_count] <= #1 sample[0];
-	    else
-	      sbuf_rxd_tmp[re_count] <= #1 sample[1];
-	  end else begin
-	    sample[sam_cnt[1:0]] <= #1 rxd;
-	  end
-    sam_cnt <= #1 sam_cnt + 1'b1;
-	end
+        if (((pcon[7]) & (smod_cnt_r==6'd31)) | (!(pcon[7]) & (smod_cnt_r==6'd63))) begin
+          r_int <= #1 1'b0;
+    	    re_count <= #1 re_count + 4'd1;
+          smod_cnt_r <= #1 6'h00;
+          sbuf_rxd_tmp[re_count_buff] <= #1 rxd;
+          re_count <= #1 re_count + 4'd1;
+	      end else begin
+          smod_cnt_r <= #1 smod_cnt_r + 6'h1;
+	        if (re_count==4'd11) begin
+	          sbuf_rxd <= #1 sbuf_rxd_tmp[8:1];
+	          r_int <= #1 sbuf_rxd_tmp[0] | !scon[5];
+	          receive <= #1 1'b0;
+	        end else 
+	          r_int <= #1 1'b0;
+        end
       end
       default: begin // mode 3
         if ((t1_ow) & !(t1_ow_buf))
-	begin
-	  if ((pcon[7]) | (smod_cnt_r))
-	  begin
-            sam_cnt <= #1 3'b000;
-
-            if (re_count==4'd11) begin
-	      sbuf_rxd <= #1 sbuf_rxd_tmp[8:1];
-	      receive <= #1 1'b0;
-	      r_int <= #1 sbuf_rxd_tmp[0] | !scon[5];
-	    end else begin
-	      sam_cnt <= #1 3'b000;
-	      r_int <= #1 1'b0;
-	    end
-
-	    re_count <= #1 re_count + 4'd1;
-	    smod_cnt_r <= #1 1'b0;
-	  end else smod_cnt_r <= #1 1'b1;
-	end else begin
-	  r_int <= #1 1'b0;
-	  if (sam_cnt==3'b011)
-	    if ((sample[0] ^ sample[1]) | (sample[0] ^ sample[2]))
-	      sbuf_rxd_tmp[re_count] <= #1 sample[0];
-	    else
-	      sbuf_rxd_tmp[re_count] <= #1 sample[1];
-	  else begin
-	    sample[sam_cnt[1:0]] <= #1 rxd;
-	    sam_cnt <= #1 sam_cnt + 1'b1;
-	  end
-	end
+        begin
+          if (((pcon[7]) &  (smod_cnt_r == 6'd15))| (!(pcon[7]) & (smod_cnt_r==6'd31)))
+	        begin
+            sbuf_rxd_tmp[re_count] <= #1 rxd;
+	          r_int <= #1 1'b0;
+  	        re_count <= #1 re_count + 4'd1;
+	          smod_cnt_r <= #1 6'h00;
+	        end else smod_cnt_r <= #1 smod_cnt_r + 6'h01;
+	      end else begin
+          if (re_count==4'd11) begin
+            sbuf_rxd <= #1 sbuf_rxd_tmp[8:1];
+            receive <= #1 1'b0;
+	          r_int <= #1 sbuf_rxd_tmp[0] | !scon[5];
+	        end else begin
+            r_int <= #1 1'b0;
+          end
+	      end
       end
     endcase
   end else begin
     case (scon[7:6])
       2'b00: begin
-        if ((scon[4]) & !(scon[0]) & !(r_int)) begin
+        if ((scon[4]) && !(scon[0]) && !(r_int)) begin
           receive <= #1 1'b1;
         end
       end 
       2'b10: begin
-        if ((rxd_buf) & !(rxd)) begin
+        if ((scon[4]) && !(rxd)) begin
           receive <= #1 1'b1;
+          if (pcon[7])
+            smod_cnt_r <= #1 6'd15;
+          else smod_cnt_r <= #1 6'd31;
         end  
       end
       default: begin 
-        if ((rxd_buf) & !(rxd)) begin
-          re_start <= #1 1'b1;
-        end else if ((re_start) & (t1_ow) & !(t1_ow_buf)) begin
-          re_start <= #1 1'b0;
-          receive <= 1'b1;
+        if ((scon[4]) && (!rxd)) begin
+          if (pcon[7])
+            smod_cnt_r <= #1 6'd7;
+          else smod_cnt_r <= #1 6'd15;
+          receive <= #1 1'b1;
         end
       end
     endcase
     
-    sample <= #1 3'b000;
-    sam_cnt <= #1 3'b000;
-    re_count <= #1 4'd0;
     sbuf_rxd_tmp <= #1 11'd0;
+    re_count <= #1 4'd0;
     r_int <= #1 1'b0;
   end
 end
@@ -391,4 +358,10 @@ begin
     bit_out <= #1 scon[rd_addr[2:0]];
 end
 
+always @(posedge clk or posedge rst)
+  if (rst)
+    re_count_buff <= #1 4'h4;
+  else re_count_buff <= #1 re_count;
+
 endmodule
+
